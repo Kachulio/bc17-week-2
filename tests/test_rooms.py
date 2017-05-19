2625fd0532078d02c8c4ea2e8efa3d15a2e70af2
kachulio1 <jngugi88@gmail.com>
2017-05-19 13:19:50 +0300

    added some tests

--- tests/test_rooms.py
+++ tests/test_rooms.py
@@ -1,25 +1,51 @@
 import unittest
 
 from intelliJo.amity.amity import Amity
-from intelliJo.room.room import Room
+from intelliJo.person.person import Fellow, Staff
+
 
 
 class TestCreateRoom(unittest.TestCase):
 
     def setUp(self):
-        self.dojo = Amity()
+        self.oscar = Amity()
+        self.office_count = len(Amity.office_rooms)
+        self.living_space_count = len(Amity.free_living_space)
+
+        self.people = [
+            'John',
+            'Awesome',
+            'Oti',
+            'Ben',
+            'Grace',
+            'Monica',
+            'Helen',
+
+        ]
 
 
-    def test_create_room_successfully(self):
-        my_class_instance = self.dojo
-        initial_room_count = Room.all_rooms
-        blue_office = my_class_instance.create_room("Blue", "office")
-        self.assertTrue(blue_office)
+    def test_create_office_successfully(self):
+        self.oscar.create_office("Blue")
+        self.assertTrue(self.oscar.office_rooms)
 
-        self.assertEqual(Room.all_rooms - initial_room_count, 1)
 
-    def test_how_many_room(self):
+    def test_create_living_space_successfully(self):
+        self.oscar.create_living_space("Red")
+        self.assertTrue(self.oscar.free_living_space)
+
+
+    def test_max_capacity_for_office(self):
+
+
         pass
+
+    def test_max_capacity_for_living_space(self):
+
+        pass
+
+    def test_print_room_returns_expected_message(self):
+        val = self.oscar.print_room('b')
+        self.assertEqual('there is no room named b',val )
 
 
 if __name__ == '__main__':

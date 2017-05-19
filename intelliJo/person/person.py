748361b1c057ac35389fe8a9c17ba5d8f5dd1435
kachulio1 <jngugi88@gmail.com>
2017-05-19 14:43:07 +0300

    added the staff and the fellow to person script

--- intelliJo/person/person.py
+++ intelliJo/person/person.py
@@ -4,7 +4,24 @@ class Person:
         self.first_name = first_name
         self.last_name = last_name
 
-
-    def get_full_name(self):
+    @property
+    def full_name(self):
         return self.first_name + " " + self.last_name
 
+
+class Fellow(Person):
+    def __init__(self, first_name, last_name):
+        super().__init__(
+            first_name,
+            last_name,
+        )
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+class Staff(Person):
+    def __init__(self, first_name, last_name):
+        super().__init__(
+            first_name,
+            last_name,
+        )

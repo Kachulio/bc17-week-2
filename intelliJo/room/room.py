8e5f00c4ca1427eb1c0bb387086e2b42bc01b921
kachulio1 <jngugi88@gmail.com>
2017-05-19 15:07:39 +0300

    added the livingspace class and office class

--- intelliJo/room/room.py
+++ intelliJo/room/room.py
@@ -1,7 +1,28 @@
 class Room:
-    # class variable
-    all_rooms = 0
 
     def __init__(self, room_name):
         self.room_name = room_name
-        Room.all_rooms += 1
+
+    @property
+    def room_type(self):
+        return self.__class__.__name__
+
+
+class LivingSpace(Room):
+    def __init__(self, room_name):
+        super().__init__(room_name)
+        self.max_capacity = 4
+        self.people = []
+
+    def __str__(self):
+        return "Living Space: " + self.room_name
+
+class Office(Room):
+    def __init__(self, room_name):
+        super().__init__(room_name)
+        self.people = []
+        self.max_capacity = 6
+
+
+    def __str__(self):
+        return "Office: " + (self.room_name)

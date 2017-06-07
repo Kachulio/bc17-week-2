04eebcb00cf7bad1567551e933ddfe5b25ead332
kachulio1 <jngugi88@gmail.com>
2017-06-07 09:42:28 +0300

    add method to check if the room is full

--- intelliJo/room/room.py
+++ intelliJo/room/room.py
@@ -8,17 +8,23 @@ class Room:
         return self.__class__.__name__
 
 
-    def insert_user(self, usr):
-        if len(self._people) == self.max_capacity:
+    def insert_user(self, user):
+        if len(self.people) == self.max_capacity:
             return "The office {} is full".format(self.room_name)
         else:
-            self._people.append(usr)
+            self.people.append(user)
+            return 'user added successfully'
+
+
 
 class LivingSpace(Room):
     def __init__(self, room_name):
         super().__init__(room_name)
         self.max_capacity = 4
-        self._people = []
+        self.people = []
+
+    def is_full(self):
+        return self.max_capacity == self.people
 
     def __str__(self):
         return "Living Space: " + self.room_name
@@ -26,8 +32,11 @@ class LivingSpace(Room):
 class Office(Room):
     def __init__(self, room_name):
         super().__init__(room_name)
-        self._people = []
+        self.people = []
         self.max_capacity = 6
+
+    def is_full(self):
+        return self.max_capacity == self.people
 
     def __str__(self):
         return "Office: " + (self.room_name)
